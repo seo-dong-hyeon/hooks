45dc052ee0cfeb4cc8acd33f102c02ba2b159e88
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-16 20:32:53 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+//import { render } from "react-dom";
 
-export default function App() {
+const App = () => {
+  const [item, setItem] = useState(0);
+  const increment = () => setItem(item + 1);
+  const decrement = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello fuck {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
+};
+
+export default App;
+
+/*class UglyApp extends React.Component {
+  state = {
+    item: 0
+  };
+
+  increment = () => {
+    this.setState(state => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+
+  decrement = () => {
+    this.setState(state => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
+
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello fuck you {item}</h1>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+      </div>
+    );
+  }
 }
+export default UglyApp;*/
