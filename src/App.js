6264922a3e3d056860ebde88a8dd41124c866b09
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-16 21:34:21 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,53 +1,32 @@
 import React, { useState } from "react";
 import "./styles.css";
-//import { render } from "react-dom";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if(typeof validator === "function"){
+      willUpdate = validator(value);
+    }
+    if(willUpdate){
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
 
 const App = () => {
-  const [item, setItem] = useState(0);
-  const increment = () => setItem(item + 1);
-  const decrement = () => setItem(item - 1);
+  const specialChar = value => !value.includes("@");
+  const name = useInput("Mr.",specialChar);
   return (
     <div className="App">
-      <h1>Hello fuck {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+      <h1>Hello fuck</h1>
+      <input placeholder="name" value={name.value} onChange={name.onChange} />
     </div>
   );
 };
 
 export default App;
-
-/*class UglyApp extends React.Component {
-  state = {
-    item: 0
-  };
-
-  increment = () => {
-    this.setState(state => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-
-  decrement = () => {
-    this.setState(state => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello fuck you {item}</h1>
-        <button onClick={this.increment}>increment</button>
-        <button onClick={this.decrement}>decrement</button>
-      </div>
-    );
-  }
-}
-export default UglyApp;*/
